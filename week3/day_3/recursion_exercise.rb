400f99468b0366daa8ba80ca5a95095c05143915
John Cigale <jcigale98@gmail.com>
2021-10-21 11:23:01 -0400

    w3d4

--- week3/day_3/recursion_exercise.rb
+++ week3/day_3/recursion_exercise.rb
@@ -1,1 +1,27 @@
-#test
+class Array
+    def deep_dup
+        duped = []
+        self.each do |ele|
+            if ele.is_a?(Array)
+                duped << ele.deep_dup
+            else
+                duped << ele
+            end
+        end
+        duped
+    end
+
+    
+end
+
+# robot_parts = [
+#   ["nuts", "bolts", "washers"],
+#   ["capacitors", "resistors", "inductors"]
+# ]
+
+# robot_parts_copy = robot_parts.deep_dup
+
+# # shouldn't modify robot_parts
+# p robot_parts_copy[1] << "LEDs"
+# # but it does
+# p robot_parts[1] # => ["capacitors", "resistors", "inductors", "LEDs"]
